5fea458544107dd4228b64ad71f3aa0bf826c139
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-08 23:22:04 -0300

    FEAT: Implement new methods on controller

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,11 +1,28 @@
-import { Controller, Post } from '@nestjs/common';
+import { Player } from './interfaces/player.interface';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/create-player.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
   @Post()
-  async createUpdatePlayer() {
-    return JSON.stringify({
-      name: 'Carlos',
-    });
+  async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    await this.playersService.createUpdatePlayer(createPlayerDto);
+  }
+
+  @Get()
+  async getPlayers(@Query('email') email: string): Promise<Player[] | Player> {
+    if (email) {
+      return await this.playersService.getPlayerByEmail(email);
+    } else {
+      return await this.playersService.getPlayers();
+    }
+  }
+
+  @Delete()
+  async deletePlayer(@Query('email') email: string): Promise<void> {
+    this.playersService.delete(email);
   }
 }
